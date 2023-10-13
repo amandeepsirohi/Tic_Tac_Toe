8c3f9cd46ab2e40e852d2177e5df1d51aa4b9e0a
krishna <krishna404@yandex.com>
2023-10-13 15:42:20 +0530

    Added Tailwind and rewrote the Game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,2 @@
-import './App.css';
-import TicTacToe from './Components/TicTacToe/TicTacToe';
-
-
-function App() {
-  return (
-    <div>
-      <TicTacToe />
-    </div>
-  );
-}
-
-export default App;
+import TicTacToe from "./Components/TicTacToe/TicTacToe";
+export default TicTacToe;
